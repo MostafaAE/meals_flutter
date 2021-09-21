2b3c45c0399d86d577126306cf004507b1a6de21
Mostafa Ayman <mostafa.aymanae@gmail.com>
2021-09-21 03:54:51 +0200

    tabs screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_flutter/screens/category_meals_screen.dart';
 import 'package:meals_flutter/screens/meal_detail_screen.dart';
-
-import './screens/categories_screen.dart';
+import 'package:meals_flutter/screens/tabs_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -34,7 +33,7 @@ class MyApp extends StatelessWidget {
           ),
       ),
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import './categories_screen.dart';
+import './favorite_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {'page': CategoriesScreen(), 'title': 'Categories'},
+    {'page': FavoriteScreen(), 'title': 'Your Favorite'},
+  ];
+  int _selectedPage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPage]['title'] as String),
+      ),
+      body: _pages[_selectedPage]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Theme.of(context).accentColor,
+        unselectedItemColor: Colors.white,
+        currentIndex: _selectedPage,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedPage = index;
+          });
+        },
+      ),
+    );
+  }
+}
